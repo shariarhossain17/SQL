74d993b0da3d6e12b3b2334c4b868ebf2b529ee6
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-27 08:20:54 +0600

    sql data type

---
+++ query/data_types.sql
@@ -0,0 +1,7 @@
+BOOLEAN -- true/false
+CHARACTER -- char/varchar/text
+NUMBERS -- float / INT
+    --int: small int (2bytes)/int(4bytes)/serial(auto increment by 1)     
+    --float: float(n)/numeric(2parameter)
+JSON/JSONB
+DATE -- DATE/TIME/TIMESTAMP/TIMESTAMPZ;
