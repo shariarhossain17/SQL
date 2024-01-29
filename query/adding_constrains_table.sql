470cccdc9fb684a21c95f8e4da9f7309fd40d3be
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-29 11:48:01 +0600

    user insert and get

--- query/adding_constrains_table.sql
+++ query/adding_constrains_table.sql
@@ -6,12 +6,23 @@
 CREATE TABLE users(
     user_id SERIAL PRIMARY KEY,
     username VARCHAR(255) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL
+    email VARCHAR(255) UNIQUE NOT NULL,
+    age int DEFAULT 18
 )
 
-CREATE TABLE users1(
-    user_id SERIAL,
-    username VARCHAR(255) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    PRIMARY KEY(user_id,username)
-)
+-- CREATE TABLE users1(
+--     user_id SERIAL,
+--     username VARCHAR(255) UNIQUE NOT NULL,
+--     email VARCHAR(255) UNIQUE NOT NULL,
+--     PRIMARY KEY(user_id,username),
+--     age int DEFAULT 18
+-- )
+
+
+-- insert USER
+INSERT INTO users VALUES(1,'abc','abc@gmail.com')
+
+
+-- get users
+
+SELECT * from users;
