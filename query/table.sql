5cc187f55da2452fbbd11b51f0bde2b68f7f792b
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-29 11:13:44 +0600

    table rename

--- query/table.sql
+++ query/table.sql
@@ -1,9 +1,14 @@
 -- Active: 1706320321800@@127.0.0.1@5432@test1
 
-
+-- create table
 CREATE TABLE student  (
     student_id INT,
     first_name VARCHAR( 50),
     last_name VARCHAR(50),
     cgpa NUMERIC(1,2)
 )
+
+--Rename TABLE
+ALTER TABLE student RENAME TO students
+
+
