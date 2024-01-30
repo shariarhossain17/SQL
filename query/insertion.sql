fbd6179f4a61e874b5bbdae8f94ac93b5d9e694c
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-30 08:13:01 +0600

    insert way no 3 and get data

--- query/insertion.sql
+++ query/insertion.sql
@@ -20,3 +20,12 @@ TRUNCATE TABLE user2;
 -- way 2
 
 INSERT INTO user2(email,first_name) VALUES ('sh@gmail.com','sh'),('sh2@gmail.com','sh2');
+
+
+--way 3
+INSERT INTO user2(user_id,email,first_name) VALUES (1,'sh@gmail.com','sh'),(2,'sh2@gmail.com','sh2');
+
+SELECT  * FROM user2 
+
+
+
