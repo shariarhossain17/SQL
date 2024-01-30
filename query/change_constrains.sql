b3eb51634358eac47b0e538a010eeb0eb69360ad
shariarhossain17 <shariarhossain23@gmail.com>
2024-01-30 08:18:28 +0600

    change constrains syntax

---
+++ query/change_constrains.sql
@@ -0,0 +1,18 @@
+-- Active: 1706320321800@@127.0.0.1@5432@test1
+
+-- ALTER TABLE
+
+-- add a column,DROP column,change datatype of a COLUMN
+-- RENAME a column,set DEFAULT value a COLUMN
+-- add CONSTRAINT to a COLUMN,drop CONSTRAINT a COLUMN
+-- table RENAME
+
+
+-- syntax ALTER TABLE action
+
+CREATE TABLE "user2"(
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE  NOT NULL,
+    first_name VARCHAR(255) UNIQUE NOT NULL,
+    age INT DEFAULT 18
+)
