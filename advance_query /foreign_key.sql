b129a94c7ecbed95e983179b5aa78de2cbf4c146
shariarhossain17 <shariarhossain23@gmail.com>
2024-02-01 00:55:51 +0600

    insert data

--- advance_query /foreign_key.sql
+++ advance_query /foreign_key.sql
@@ -1,5 +1,8 @@
 -- Active: 1706320321800@@127.0.0.1@5432@test1
 CREATE TABLE Department(
     deptId SERIAL PRIMARY KEY,
-    dept_name VARCHAR(255) UNIQUE,
-)
+    dept_name VARCHAR(255) UNIQUE
+);  
+
+
+INSERT INTO Department VALUES (1,'Psychology');
