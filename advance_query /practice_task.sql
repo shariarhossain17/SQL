4f4436f825ce0b4d2612217890362a2d840b6bab
shariarhossain17 <shariarhossain23@gmail.com>
2024-03-19 02:44:06 +0600

    task2 complete

--- advance_query /practice_task.sql
+++ advance_query /practice_task.sql
@@ -23,3 +23,28 @@ VALUES (1, 'John Doe', 'HR', 50000.00),
 --  task 1 Write an SQL query to retrieve the names and salaries of employees who work in the "HR" department.
 
 SELECT emp_name, salary FROM employee WHERE department = 'HR';
+
+-- task2 Task 2: Aggregation and HAVING Clause Create a table named "orders" with columns (order_id, customer_id, total_amount) and insert the following data:
+
+CREATE TABLE orders (
+    order_id INT PRIMARY KEY, customer_id INT, total_amount DECIMAL(10, 2)
+)
+
+INSERT INTO
+    orders (
+        order_id, customer_id, total_amount
+    )
+VALUES (101, 1, 200.00),
+    (102, 2, 300.00),
+    (103, 1, 150.00),
+    (104, 3, 400.00),
+    (105, 2, 250.00);
+
+SELECT * FROM orders;
+
+SELECT customer_id, avg(total_amount) as total_amount
+from orders
+GROUP BY
+    customer_id
+HAVING
+    avg(total_amount) >= 250;
