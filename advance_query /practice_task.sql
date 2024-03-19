351fd68c5439ae43b05c2c2b24ffccd0319a1714
shariarhossain17 <shariarhossain23@gmail.com>
2024-03-19 22:44:54 +0600

    inner join task 3

--- advance_query /practice_task.sql
+++ advance_query /practice_task.sql
@@ -48,3 +48,48 @@ GROUP BY
     customer_id
 HAVING
     avg(total_amount) >= 250;
+
+-- Task 3: JOIN and GROUP BY Create two tables named "students" and "courses" with columns as follows:
+
+CREATE TABLE students (
+    student_id INT PRIMARY KEY, student_name VARCHAR(50), age INT, gender VARCHAR(10)
+)
+
+CREATE TABLE courses (
+    course_id INT PRIMARY KEY, course_name VARCHAR(50), credits INT
+)
+
+INSERT INTO
+    students (
+        student_id, student_name, age, gender
+    )
+VALUES (1, 'Alice', 22, 'Female'),
+    (2, 'Bob', 21, 'Male'),
+    (3, 'Charlie', 23, 'Male');
+
+INSERT INTO
+    courses (
+        course_id, course_name, credits
+    )
+VALUES (101, 'Mathematics', 3),
+    (102, 'History', 4),
+    (103, 'Physics', 3);
+
+CREATE TABLE enrollment (
+    enrollment_id INT PRIMARY KEY, student_id INT, course_id INT
+)
+
+INSERT INTO
+    enrollment (
+        enrollment_id, student_id, course_id
+    )
+VALUES (1, 1, 101),
+    (2, 1, 102),
+    (3, 2, 103),
+    (4, 3, 101);
+
+SELECT s.student_name, course_name, credits
+FROM
+    students s
+    INNER JOIN enrollment e on e.student_id = s.student_id
+    INNER JOIN courses cr on cr.course_id = e.course_id;
